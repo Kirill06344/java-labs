e3bef6a8f1d2abacf3395776ef52a45da631d0ed
dasu <kirill12093@mail.ru>
2023-03-08 18:21:50 +0300

    fix Annotation + add variative to params

--- src/tasks/annotation/DemoClass.java
+++ src/tasks/annotation/DemoClass.java
@@ -2,31 +2,31 @@ package tasks.annotation;
 public class DemoClass {
 
     @MyAnnotation(count = 1)
-    public static void method1(int a, int b) {
+    public static void method1(double a, int b) {
         System.out.println("Parameters: a=" + a + " ,b=" + b);
     }
 
     @MyAnnotation
-    public static void method2(int a, int b, int c) {
+    public static void method2(String a, char b, int c) {
         System.out.println("Parameters: a=" + a + " ,b=" + b + ",c=" + c);
     }
 
     @MyAnnotation(count = 3)
-    protected static void method3(int a, int b, int c) {
+    protected static void method3(float a, byte b, int c) {
         System.out.println("Parameters: a=" + a + " ,b=" + b + ",c=" + c);
     }
 
     @MyAnnotation(count = 4)
-    protected static void method4(int a, int b) {
+    protected static void method4(boolean a, int b) {
         System.out.println("Parameters: a=" + a + " ,b=" + b);
     }
 
     @MyAnnotation(count = 2)
-    private static void method5(int a, int b, int c) {
+    private static void method5(int a, float b, String c) {
         System.out.println("Parameters: a=" + a + " ,b=" + b + ",c=" + c);
     }
     @MyAnnotation(count = 1)
-    private static void method6(int a, int b) {
+    private static void method6(boolean a, boolean b) {
         System.out.println("Parameters: a=" + a + " ,b=" + b);
     }
 }
--- src/tasks/annotation/Main.java
+++ src/tasks/annotation/Main.java
@@ -2,38 +2,71 @@ package tasks.annotation;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) {
         Class<DemoClass> demoClass = DemoClass.class;
         Method[] methods = demoClass.getDeclaredMethods();
         invokeMethods(methods);
     }
 
-    public static void invokeMethods(Method [] methods) throws InvocationTargetException, IllegalAccessException {
+    public static void invokeMethods(Method [] methods){
         for (Method method : methods) {
             if (method.isAnnotationPresent(MyAnnotation.class)) {
                 System.out.println("Calling method with name: " + method.getName());
                 System.out.println("This method has got @MyAnnotation");
-                boolean isPrivateMethod = false;
-                if (!method.canAccess(null)) {
-                    method.setAccessible(true);
-                    isPrivateMethod = true;
-                }
-                int count = method.getAnnotation(MyAnnotation.class).count();
-                System.out.println("Call method: " + method.getName() + " for " + count + " times");
-                while (count-- > 0) {
-                    if (method.getParameterCount() == 2) {
-                        method.invoke(null, 1, 2);
-                    } else {
-                        method.invoke(null, 1, 2, 3);
+                boolean isAccessibleDefault = true;
+                try {
+                    if (!Modifier.isPublic(method.getModifiers())) {
+                        method.setAccessible(true);
+                        isAccessibleDefault = false;
                     }
+                    int count = method.getAnnotation(MyAnnotation.class).count();
+                    System.out.println("Call method: " + method.getName() + " for " + count + " times");
+                    while (count-- > 0) {
+                        Object[] parameters = getParametersForMethod(method);
+                        method.invoke(null, parameters);
+                    }
+                } catch (IllegalAccessException  e) {
+                    System.out.println(e.getMessage());
+                } catch (InvocationTargetException e) {
+                    Throwable x = e.getCause();
+                    System.out.println("invocation of method: " + method.getName() + " failed because of " + x.getMessage());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Illegal argument for method: " + method.getName());
                 }
-                if (!isPrivateMethod) {
-                    method.setAccessible(false);
+                finally {
+                    if(!isAccessibleDefault) {
+                        method.setAccessible(false);
+                    }
                 }
                 System.out.println("-----------------------------------");
             }
         }
+    }
+
+    public static Object [] getParametersForMethod (Method method) {
+        Object[] parametres = new Object[method.getParameterCount()];
+        for (int i = 0; i < parametres.length; ++i) {
+            if (method.getParameters()[i].getType().equals(int.class)) {
+                parametres[i] = ThreadLocalRandom.current().nextInt();
+            } else if (method.getParameters()[i].getType().equals(double.class)) {
+                parametres[i] = ThreadLocalRandom.current().nextDouble();
+            } else if (method.getParameters()[i].getType().equals(boolean.class)) {
+                parametres[i] = ThreadLocalRandom.current().nextBoolean();
+            } else if(method.getParameters()[i].getType().equals(char.class)) {
+                parametres[i] = (char)(ThreadLocalRandom.current().nextInt(0, 200));
+            } else if (method.getParameters()[i].getType().equals(float.class)) {
+                parametres[i] = ThreadLocalRandom.current().nextFloat();
+            } else if (method.getParameters()[i].getType().equals(byte.class)) {
+                parametres[i] = (byte)ThreadLocalRandom.current().nextInt(-128, 127);
+            } else {
+                parametres[i] = "Random string!";
+            }
+        }
+        return parametres;
     }
 }
